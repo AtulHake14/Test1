44c516a528ed3624991374ce3398902766013438
Atul Hake <Atul@systenics.net>
2022-07-28 16:40:04 +0530

    wft

---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,24 @@
+package qaclickacademy;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class SeleniumTest {
+
+	//testng xml file from Maven
+	// how to execute all test cases from Test folder  - mvn test
+	@Test
+	public void BrowserAUtomation()
+	{
+		System.out.println("BrowserAUtomation");	
+		Assert.assertTrue(false);
+	}
+	@Test
+	public void elementsUi()
+	{
+		System.out.println("elementsUi");	
+	}
+	
+	
+}
